92be16b0fabb8f29d01b81b6329374b4e8380ac0
stsenev <124162418+stsenev@users.noreply.github.com>
2026-03-27 13:36:55 +0400

    Delete reports directory

--- reports/__init__.py
+++
@@ -1,2 +0,0 @@
-# reports/__init__.py
-default_app_config = 'reports.apps.ReportsConfig'
--- reports/utils.py
+++
@@ -1,5 +0,0 @@
-# reports/utils.py
-# Этот файл теперь будет реэкспортировать функции из period_utils.py
-from .period_utils import check_period_availability, get_previous_approved_report
-
-__all__ = ['check_period_availability', 'get_previous_approved_report']
